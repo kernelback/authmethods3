2626ac5d6a910b4ae4dde8fa768be5bbfdd6d97a
Kernel <86235471+kernelback@users.noreply.github.com>
2025-04-22 19:32:09 +0200

    Ajout paramètre de connexionGitHub via OAuth

--- src/Security/OAuthUserService.php
+++ src/Security/OAuthUserService.php
@@ -8,16 +8,22 @@ use Doctrine\ORM\EntityManagerInterface;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class OAuthUserService implements OAuthAwareUserProviderInterface
 {
     private UserRepository $userRepository;
     private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $passwordHasher;
 
-    public function __construct(UserRepository $userRepository, EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ) {
         $this->userRepository = $userRepository;
         $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
     }
 
     public function loadUserByOAuthUserResponse(UserResponseInterface $response): UserInterface
@@ -29,7 +35,19 @@ class OAuthUserService implements OAuthAwareUserProviderInterface
         if (!$user) {
             $user = new User();
             $user->setEmail($email);
-            $user->setRoles(['ROLE_USER']);
+
+            $fullName = $response->getRealName() ?? $response->getNickname() ?? 'GitHub User';
+            $username = $response->getNickname() ?? explode('@', $email)[0];
+
+            $user->setFullName($fullName);
+            $user->setUsername($username);
+            $user->setRoles(['ROLE_ADMIN']);
+
+            // ✅ Mot de passe factice requis par la BDD
+            $fakePassword = bin2hex(random_bytes(10));
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $fakePassword);
+            $user->setPassword($hashedPassword);
+
             $this->entityManager->persist($user);
             $this->entityManager->flush();
         }
